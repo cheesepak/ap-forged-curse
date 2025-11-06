ba7e94d346247b4bb72e64a796a982e6c54001fe
Red Makes Things <cheree.andringa@gmail.com>
2025-11-05 22:37:49 -0800

    Update name of logic

--- forgedcurse/Options.py
+++ forgedcurse/Options.py
@@ -2,9 +2,9 @@ import typing
 from dataclasses import dataclass
 from Options import DeathLink, Toggle, Range, PerGameCommonOptions
 
-class SkipsLogic(Toggle):
+class BlinkRodSkips(Toggle):
     """"Adds unintended blink rod skips and tanking spikes logic"""
-    display_name = "Skips Logic"
+    display_name = "Blink Rod Skips"
 
 class DeathLink(Toggle):
     """Adds death link"""
@@ -12,5 +12,5 @@ class DeathLink(Toggle):
 
 @dataclass
 class FCOptions(PerGameCommonOptions):
-    skips_logic: SkipsLogic
+    blink_rod_skips: BlinkRodSkips
     death_link: DeathLink
